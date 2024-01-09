44df4d407b1aaafc2e12e1c170bfad0632fd80d7
Veru <43966969+VeruAmari@users.noreply.github.com>
2024-01-09 17:40:32 -0300

    Implement available ship object tracker

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,61 +1,74 @@
+import shipFactory from './shipFact';
+
 /* eslint no-plusplus: off */
-const graph = () => {
-  const moves = [
-    [1, 0],
-    [0, 1],
-    [-1, 0],
-    [0, -1],
-  ];
-  const vertex = (x, y) => {
-    const coordinates = [x, y];
-    // eslint-disable-next-line prefer-const
-    let shipID = null;
-    return { coordinates, shipID };
-  };
-  const defineVertices = () => {
-    const vertices = {};
-    for (let i = 0; i < 10; i++) {
-      for (let j = 0; j < 10; j++) {
-        vertices[`${i},${j}`] = vertex(i, j);
+const gameboard = () => {
+  const graph = () => {
+    const moves = [
+      [1, 0],
+      [0, 1],
+      [-1, 0],
+      [0, -1],
+    ];
+    const vertex = (x, y) => {
+      const coordinates = [x, y];
+      // eslint-disable-next-line prefer-const
+      let shipID = null;
+      return { coordinates, shipID };
+    };
+    const defineVertices = () => {
+      const vertices = {};
+      for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+          vertices[`${i},${j}`] = vertex(i, j);
+        }
       }
+      return vertices;
+    };
+
+    const adjacencyList = (vertexList, movesList) => {
+      const adjacency = {};
+      Object.keys(vertexList).forEach((key) => {
+        adjacency[key] = {};
+        movesList.forEach((move) => {
+          const moveKey = `${Number(key[0]) + Number(move[0])},${
+            Number(key[2]) + Number(move[1])
+          }`;
+          if (vertexList[moveKey]) {
+            adjacency[key][moveKey] = vertexList[moveKey];
+          }
+        });
+      });
+      return adjacency;
+    };
+    return { adjacencyList, defineVertices, moves };
+  };
+
+  // const boardGraph = graph();
+
+  // const myShips = {};
+  const makeShips = (size, amount, id) => {
+    const myShips = {};
+    for (let i = 0; i < amount; i++) {
+      myShips[`${id + (i + 1)}`] = shipFactory(size);
     }
-    return vertices;
+    return { ...myShips };
   };
-
-  /*
-  const edgeList = (vertexList, movesList) => {
-    const edgeObj = (inVertex, outVertex) => ({ inVertex, outVertex });
-    const edges = new Set();
-    Object.keys(vertexList).forEach((key) => {
-      movesList.forEach((move) => {
-        const k = `${Number(key[0]) + Number([move[0]])},${
-          Number(key[2]) + Number(move[1])
-        }`;
-        if (vertexList[k]) {
-          edges.add(edgeObj(vertexList[key], vertexList[k]));
-        }
-      });
-    });
-    return edges;
+  /** Rules:
+   * No. 	Class of ship 	Size
+      1 	Carrier 	      5
+      2 	Battleship    	4
+      3 	Destroyer 	    3
+      4 	Submarine 	    3
+      5 	Patrol Boat 	  2 
+   */
+  const ships = {
+    ...makeShips(5, 1, 'c'),
+    ...makeShips(4, 2, 'b'),
+    ...makeShips(3, 3, 'd'),
+    ...makeShips(3, 4, 's'),
+    ...makeShips(2, 5, 'pb'),
   };
-*/
-  const adjacencyList = (vertexList, movesList) => {
-    const adjacency = {};
-    Object.keys(vertexList).forEach((key) => {
-      adjacency[key] = {};
-      movesList.forEach((move) => {
-        const moveKey = `${Number(key[0]) + Number(move[0])},${
-          Number(key[2]) + Number(move[1])
-        }`;
-        if (vertexList[moveKey]) {
-          adjacency[key][moveKey] = vertexList[moveKey];
-        }
-      });
-    });
-    return adjacency;
-  };
-  return { adjacencyList, defineVertices, moves };
+  return { graph, ships };
 };
-const gameboard = () => ({ size: 0 });
 
-export { gameboard, graph };
+export default gameboard;
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,16 +1,17 @@
 /* eslint no-undef: off */
-import { gameboard, graph } from '../src/gameboard';
-
-test('basic test gameboard', () => {
-  expect(gameboard()).toEqual({ size: 0 });
-});
+import gameboard from '../src/gameboard';
 
 test('basic test graph', () => {
-  const myGraph = graph();
+  const myGraph = gameboard().graph();
   expect(
     myGraph.adjacencyList(myGraph.defineVertices(), myGraph.moves)['0,0'],
   ).toEqual({
     '1,0': { coordinates: [1, 0], shipID: null },
     '0,1': { coordinates: [0, 1], shipID: null },
   });
+});
+
+test('basic test ships', () => {
+  const theShips = gameboard().ships;
+  expect(Object.keys(theShips).length).toEqual(15);
 });
